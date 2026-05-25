6789518f2a81559c7be896a465dccc44c5eaaca8
Youssef <yabdelmonem5@gmail.com>
2026-05-25 23:32:42 +0300

    first commit

---
+++ app/students/page.js
@@ -0,0 +1,20 @@
+export default function StudentsPage() {
+  return (
+    <main style={{
+      background: "black",
+      color: "white",
+      minHeight: "100vh",
+      padding: "40px"
+    }}>
+      
+      <h1 style={{
+        color: "yellow",
+        fontSize: "50px",
+        fontWeight: "bold"
+      }}>
+        Students
+      </h1>
+
+    </main>
+  );
+}
